268716c5e01227ca08ef0ada77e7823bf5be6e08
Jarred C <jarrywc@gmail.com>
2025-11-07 02:52:46 -0500

    supplier and manufacturer seeders

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -10,6 +10,18 @@ class SupplierSeeder extends Seeder
     public function run()
     {
         Supplier::truncate();
-        Supplier::factory()->count(5)->create();
+
+        // Manufacturers as suppliers
+        Supplier::factory()->count(1)->microsoft()->create();
+        Supplier::factory()->count(1)->apple()->create();
+        Supplier::factory()->count(1)->dell()->create();
+        Supplier::factory()->count(1)->lenovo()->create();
+        Supplier::factory()->count(1)->asus()->create();
+        Supplier::factory()->count(1)->hp()->create();
+
+        // Retailers
+        Supplier::factory()->count(1)->bestBuy()->create();
+        Supplier::factory()->count(1)->target()->create();
+        Supplier::factory()->count(1)->microCenter()->create();
     }
 }
